05b402cbda3c225c218a9996267ba2a1d46f1d70
Blog.apk <46821030+BuatApkOlshop@users.noreply.github.com>
2019-03-22 20:09:06 +0800

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+form { 
+  display: block;
+  margin-top: 0em;
+}
+<meta charset="ISO-8859-1">
+</style>
+</head>
+<body>
+<p> SELAMAT DATANG</p>
+</form>
+<form>
+    <select>
+<option value="Login">Login</option>
+        <option value="Aplikasi">Aplikasi</option>
+        <option value="Tutorial">Tutorial</option>
+        <option value="Index">Index</option>
+        <option value="Tag">Tag</option>
+    </select>
+</form>
+<form>
+<form action="/action_page.php">
+Email: <input type="text" Name="Email" value=""><br>
+Nama Depan: <input type="text" name="NamaDepan" value=""><br>
+Nama Belakang: <input type="text" name="NamaBelakang" value=""><br>
+Password: <input type="password" name="password" value=""><br><input type="reset" value="reset"><br><input type="submit" value="Submit">
+</form>
+<p>aplikasi & kolaborasi untuk Developed. </p>
+</body>
+</html>
